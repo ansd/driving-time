d098b31f233a1919545d043f9cb1e94af5a13224
David Ansari <david.ansari@sap.com>
2019-03-20 16:15:59 +0100

    Format stdout output of print command

--- cmd/print.go
+++ cmd/print.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 	"googlemaps.github.io/maps"
@@ -18,16 +19,24 @@ var printCmd = &cobra.Command{
 	Short: "Print live driving time and distance",
 	Long:  "Print live driving time and distance to stdout",
 	Run: func(cmd *cobra.Command, args []string) {
-		requestDurations(origin, destinations)
+
+		rsp, err := requestDurations(origin, destinations)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if err := printDurations(rsp); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	},
 }
 
-func requestDurations(origin string, destinations []string) {
-
+func requestDurations(origin string, destinations []string) (*maps.DistanceMatrixResponse, error) {
 	client, err := maps.NewClient(maps.WithAPIKey(apiKey))
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
 
 	req := maps.DistanceMatrixRequest{
@@ -37,16 +46,19 @@ func requestDurations(origin string, destinations []string) {
 		DepartureTime: "now",
 	}
 
-	rsp, err := client.DistanceMatrix(context.TODO(), &req)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	return client.DistanceMatrix(context.TODO(), &req)
+}
 
-	fmt.Printf("Origin: %v\n", rsp.OriginAddresses[0])
-	fmt.Printf("Destination\tStatus\tDuration\tDurationInTraffic\tDistance\n")
+func printDurations(rsp *maps.DistanceMatrixResponse) error {
+	fmt.Println()
+	fmt.Printf("Origin:\n%s\n", rsp.OriginAddresses[0])
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', tabwriter.TabIndent)
+	fmt.Fprintln(w)
+	fmt.Fprintf(w, "Destination\tStatus\tDuration\tDurationInTraffic\tDistance\n")
 	for i, dst := range rsp.DestinationAddresses {
 		elem := rsp.Rows[0].Elements[i]
-		fmt.Printf("%v\t%v\t%v\t%v\t\t%v\n", dst, elem.Status, elem.Duration, elem.DurationInTraffic, elem.Distance.HumanReadable)
+		fmt.Fprintf(w, "%v\t%v\t%v\t%v\t%v\n", dst, elem.Status, elem.Duration, elem.DurationInTraffic, elem.Distance.HumanReadable)
 	}
+	return w.Flush()
 }
